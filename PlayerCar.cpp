715f3e59378b470e5ae7e9bdad586bc702ce4888
aannleax <alex@ivliev.de>
2016-02-10 22:20:06 +0100

    Movement

--- PlayerCar.cpp
+++ PlayerCar.cpp
@@ -2,7 +2,7 @@
 #include "PlayerCar.h"
 
 
-PlayerCar::PlayerCar() : Car(100, 1000)
+PlayerCar::PlayerCar() : Car(100, 800)
 {
 	//Texture laden
 	//Mittelpunkt des Sprite als Ursprung festlegen
@@ -23,19 +23,38 @@ void PlayerCar::handleEvent(sf::Event& Event)
 	_Movement = sf::Vector2f(0, 0);
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
 		_Movement += sf::Vector2f(-1, 0);
-	} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
 		_Movement += sf::Vector2f(1, 0);
 	}
 	else {
 		_Movement = sf::Vector2f(0, 0);
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		_Movement += sf::Vector2f(0, -0.4);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		_Movement += sf::Vector2f(0, 1.3);
+	}
+	else
+	{
+		_Movement.y = 0;
 	}
 }
 
 void PlayerCar::update(float FrameTime)
 {
 	//_Movement anwenden - Car bewegen
-	if (((getPos() + _Movement * FrameTime * _Speed).x >= getWidth() / 2) && ((getPos() + _Movement * FrameTime * _Speed).x <= 800 - getWidth() /2))
+	if (((getPos() + _Movement * FrameTime * _Speed).x >= getWidth() / 2) && ((getPos() + _Movement * FrameTime * _Speed).x <= 800 - getWidth() / 2))
 	{
-		setPos(getPos() + (_Movement * FrameTime * _Speed));
+		setPos(sf::Vector2f(getPos().x + (_Movement.x * _Speed * FrameTime), getPos().y));
+	}
+
+	if (getPos().y + getHeight() / 2 + _Movement.y * FrameTime * _Speed <= 600 && getPos().y - getHeight() / 2 + _Movement.y * FrameTime * _Speed >= 0)
+	{
+		setPos(sf::Vector2f(getPos().x, getPos().y + _Movement.y * FrameTime * _Speed));
 	}
 }

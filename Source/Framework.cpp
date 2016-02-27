28d5864cc982f9ddd363a959ea7dd7948f3ec6d9
henne90gen <henne90gen@gmail.com>
2016-02-27 22:06:43 +0100

    stuff

--- Source/Framework.cpp
+++ Source/Framework.cpp
@@ -155,7 +155,6 @@ void Framework::handleEvents()
 	case GameState::Options:
 		_GameState = _OptionsMenu.handleEvents(_RenderWindow);
 		_MenuMusic.setVolume(_OptionsMenu.getVolume());
-		//std::cout << "Volume: " << _MenuMusic.getVolume() << std::endl;
 		break;
 	case GameState::GameOver:
 		_GameState = _GameOverScreen.handleEvents(_RenderWindow);

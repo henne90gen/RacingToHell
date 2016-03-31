4b57a7a75faf333795ad8db2390a7cf1e0bf0920
Alex Ivliev <alex@ivliev.de>
2016-03-31 10:27:56 +0200

    NetworkHandle

--- Headers/Multiplayer/NetworkHandle.h
+++ Headers/Multiplayer/NetworkHandle.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include <SFML\Network.hpp>
+#include "Communication.h"
+
+class NetworkHandle
+{
+public:
+	NetworkHandle();
+	~NetworkHandle() {}
+
+	bool connect(std::string ip, int port, float timeout);
+	void disconnect();
+
+	void run();
+
+	NetworkType getRelationship() { return _Relationship; }
+private:
+	sf::TcpSocket _Socket;
+	sf::TcpListener _Listener;
+
+	unsigned int _TickRate;
+	sf::Uint32 _Tick;
+	
+	NetworkType _Relationship;
+};
--- Source/Multiplayer/NetworkHandle.cpp
+++ Source/Multiplayer/NetworkHandle.cpp
@@ -1,2 +1,31 @@
 #include "stdafx.h"
 #include "Multiplayer\NetworkHandle.h"
+
+NetworkHandle::NetworkHandle() : _TickRate(128), _Relationship(NetworkType::None), _Tick(0)
+{
+}
+
+bool NetworkHandle::connect(std::string ip, int port, float timeout)
+{
+	_Socket.setBlocking(true);
+
+	return _Socket.connect(ip, port, sf::seconds(timeout)) == sf::Socket::Status::Done;
+}
+
+void NetworkHandle::disconnect()
+{
+	_Socket.disconnect();
+}
+
+void NetworkHandle::run()
+{
+	while (_Relationship != NetworkType::None)
+	{
+		_Socket.setBlocking(false);
+
+		sf::Packet TestPacket, SendPacket;
+
+		++_Tick;
+		sf::sleep(sf::seconds(1.0f / (float)_TickRate));
+	}
+}

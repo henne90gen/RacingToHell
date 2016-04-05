b1d32789b4717bc55384e9376d69c7bfb81def93
Alex Ivliev <alex@ivliev.de>
2016-04-05 20:23:29 +0200

    Credits

--- Headers/Menu/AboutScreen.h
+++ Headers/Menu/AboutScreen.h
@@ -12,7 +12,7 @@ public:
 	GameState handleMenuItemResult(MenuResult result);
 
 private:
-	sf::Text _Text2, _LevelUp, _LevelUp2, _Music, _Cars, _Boss, _Boss2;
+	sf::Text _Text2, _LevelUp, _LevelUp2, _Music, _Cars, _Boss, _Boss2, _AIShotSounds, _AIShotSounds2, _PlayerCarShotSounds, _PlayerCarShotSounds2;
 	sf::RectangleShape _Frame;
 };
 

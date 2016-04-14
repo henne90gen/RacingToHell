8d976e1e7d70441549413a3c226f7aca8f27e18f
henne90gen <henne90gen@gmail.com>
2016-04-14 11:47:45 +0200

    AICar are being updated now

--- Headers/Multiplayer/Communication.h
+++ Headers/Multiplayer/Communication.h
@@ -3,6 +3,6 @@
 enum class NetworkRelation {None, Host, Client};
 
 enum class NetworkCommunication {	None, Authentication, WrongPassword, ConnectionFailed, ConnectionSuccesfull, Disconnect, Kick, StartGame, 
-									EndGame, Ready, SettingsChanged, CreateGameObject, DeleteGameObject, UpdateP2, SynchroniseTick };
+									EndGame, Ready, SettingsChanged, CreateGameObject, DeleteGameObject, UpdateAICar, UpdateP2, SynchroniseTick };
 
 enum class NetworkState {None, Lobby, Ingame};

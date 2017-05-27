01c0393d3f9db252872e853a0e375cfed04de855
Hendrik Müller <henne90gen@gmail.com>
2017-05-27 14:13:35 +0200

    Adding some debug text to the screen

--- Font.h
+++ Font.h
@@ -4,6 +4,8 @@ namespace font {
 
 void renderText(VideoBuffer* buffer, std::string text, int x, int y);
 
+void renderText(VideoBuffer* buffer, std::string text, int x, int y, unsigned fontSize);
+
 void setFontSize(unsigned fontSizeInPixel);
 
 void loadFont(std::string fontFileName);

f9575b6ae6bc6e031bb02021169af8956f83e5cc
Alex <alex@ivliev.de>
2017-05-29 19:27:23 +0200

    The actual files containing Vector

---
+++ Math.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <math.h>
+
+namespace Math
+{
+    struct Vector2f
+    {
+        float x = 0.0f;
+        float y = 0.0f;
+    };
+
+    Vector2f operator+(const Vector2f &v1, const Vector2f &v2);
+    Vector2f operator*(float c, const Vector2f &v);
+    Vector2f operator*(const Vector2f &v, float c);
+
+    float dotProduct(const Vector2f &v1, const Vector2f &v2);
+    float length(const Vector2f &v);
+}
+

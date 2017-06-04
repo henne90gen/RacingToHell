e9a19ceff4cb2e5e0e2424d684c1c0981c31b7d5
Alex <alex@ivliev.de>
2017-06-05 00:25:27 +0200

    My Math

---
+++ src/MyMath.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <math.h>
+
+#define PI 3.141592654f
+
+namespace Math {
+struct Vector2f {
+    float x, y;
+};
+
+Vector2f operator+(const Vector2f &v1, const Vector2f &v2);
+Vector2f operator-(const Vector2f &v1, const Vector2f &v2);
+Vector2f operator*(float c, const Vector2f &v);
+Vector2f operator*(const Vector2f &v, float c);
+
+float dotProduct(const Vector2f &v1, const Vector2f &v2);
+float length(const Vector2f &v);
+Vector2f normalize(const Vector2f &v);
+}
+

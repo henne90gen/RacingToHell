0028d622e9a2344171506806cc3294fecac02d38
Hendrik Müller <henne90gen@gmail.com>
2022-10-03 17:27:33 +0200

    Made OpenGL ES 3.0 mandatory

--- android/app/src/main/java/game/racingtohell/GLSurfaceViewWrapper.java
+++ android/app/src/main/java/game/racingtohell/GLSurfaceViewWrapper.java
@@ -12,37 +12,15 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class GLSurfaceViewWrapper extends GLSurfaceView {
 
-    public GLSurfaceViewWrapper(Context context, boolean isProbablyEmulator) {
+    public GLSurfaceViewWrapper(Context context) {
         super(context);
-//        setEGLContextFactory(new EGLContextFactory() {
-//            @Override
-//            public EGLContext createContext(EGL10 egl, EGLDisplay display, EGLConfig eglConfig) {
-//                final int EGL_CONTEXT_CLIENT_VERSION = 0x3098;
-//                int[] attrib_list = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL10.EGL_NONE};
-//                return egl.eglCreateContext(display, eglConfig, EGL10.EGL_NO_CONTEXT, attrib_list);
-//            }
-//
-//            @Override
-//            public void destroyContext(EGL10 egl, EGLDisplay display, EGLContext context) {
-//                egl.eglDestroyContext(display, context);
-//            }
-//        });
-        setEGLContextClientVersion(2);
-
-        if (isProbablyEmulator) {
-            // Avoids crashes on startup with some emulator images.
-            setEGLConfigChooser(8, 8, 8, 8, 16, 0);
-        }
-
+        setEGLContextClientVersion(3);
         setRenderer(new RendererWrapper(context));
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent e) {
-        boolean pressed = true;
-        if (e.getAction() == MotionEvent.ACTION_UP) {
-            pressed = false;
-        }
+        boolean pressed = e.getAction() != MotionEvent.ACTION_UP;
         NativeWrapper.on_touch_event(e.getX(), e.getY(), pressed);
         return true;
     }
--- android/app/src/main/java/game/racingtohell/MainActivity.java
+++ android/app/src/main/java/game/racingtohell/MainActivity.java
@@ -14,48 +14,47 @@ import android.widget.Toast;
 // http://www.learnopengles.com/calling-opengl-from-android-using-the-ndk/
 public class MainActivity extends Activity {
 
-    private GLSurfaceView glSurfaceView;
-    private boolean rendererSet;
+    private GLSurfaceView glSurfaceView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityManager activityManager
-            = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
 
-        final boolean supportsEs2 =
-            configurationInfo.reqGlEsVersion >= 0x20000 || isProbablyEmulator();
+        final boolean supportsEs3 = configurationInfo.reqGlEsVersion >= 0x30000 || isProbablyEmulator();
 
         Log.i("RacingToHell", "OpenGL Version: " + configurationInfo.getGlEsVersion());
-        if (!supportsEs2) {
+        if (!supportsEs3) {
             // Should never be seen in production, since the manifest filters
             // unsupported devices.
-            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
+            Toast.makeText(this, "This device does not support OpenGL ES 3.0.",
                 Toast.LENGTH_LONG).show();
             return;
         }
 
-        glSurfaceView = new GLSurfaceViewWrapper(this, isProbablyEmulator());
-        rendererSet = true;
+        glSurfaceView = new GLSurfaceViewWrapper(this);
+        if (isProbablyEmulator()) {
+            // Avoids crashes on startup with some emulator images.
+            glSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        }
         setContentView(glSurfaceView);
     }
 
     private boolean isProbablyEmulator() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
-            && (Build.FINGERPRINT.startsWith("generic")
+        return Build.FINGERPRINT.startsWith("generic")
             || Build.FINGERPRINT.startsWith("unknown")
             || Build.MODEL.contains("google_sdk")
             || Build.MODEL.contains("Emulator")
-            || Build.MODEL.contains("Android SDK built for x86"));
+            || Build.MODEL.contains("Android SDK built for x86");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        if (rendererSet) {
+        if (glSurfaceView != null) {
             glSurfaceView.onPause();
         }
     }
@@ -64,7 +63,7 @@ public class MainActivity extends Activity {
     protected void onResume() {
         super.onResume();
 
-        if (rendererSet) {
+        if (glSurfaceView != null) {
             glSurfaceView.onResume();
         }
     }
